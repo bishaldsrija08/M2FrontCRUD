49eaa1042f2d01cfe631700011fb86683c2463a3
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-10-22 11:11:04 +0545

    Add Edit page and integrate routing; update SinglePage to link to Edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import SinglePage from "./pages/SinglePage"
+import Edit from "./pages/Edit"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/edit/:id" element={<Edit />} />
           <Route path="/single/:id" element={<SinglePage />} />
         </Routes>
       </BrowserRouter>
--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import Navbar from "../components/Navbar";
 
 const SinglePage = () => {
@@ -52,9 +52,9 @@ const SinglePage = () => {
                             </div>
                         </div>
                         <div className="flex gap-4 mt-4">
-                            <button className="flex-1 py-2.5 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200">
+                            <Link to={`/edit/${id}`} className="flex-1 py-2.5 bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-200">
                                 Edit
-                            </button>
+                            </Link>
                             <button onClick={handleDelete} className="flex-1 py-2.5 bg-red-100 hover:bg-red-200 text-red-700 font-medium rounded-lg border border-red-200 focus:outline-none focus:ring-2 focus:ring-red-300 transition duration-200">
                                 Delete
                             </button>
